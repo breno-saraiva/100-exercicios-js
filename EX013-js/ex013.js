b94d31c40bcb258ca132032a70989c9867d93867
Breno Saraiva <brenodev.saraiva@gmail.com>
2023-05-30 15:58:57 -0300

    exercício 13

---
+++ EX013-js/ex013.js
@@ -0,0 +1,3 @@
+let sal = parseFloat(prompt("qual o valor do seu salário?"))
+let novoSal = sal*1.15
+console.log("parabéns, você recebeu um aumento de 15% em seu salário passando de R$"+ sal + ", para R$" + novoSal.toFixed(2))
