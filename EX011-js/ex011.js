97926709179d6c4fb5ae405011b58ea3a7412a91
Breno Saraiva <brenodev.saraiva@gmail.com>
2023-05-30 13:36:15 -0300

    exercício 11

---
+++ EX011-js/ex011.js
@@ -0,0 +1,5 @@
+let A = parseFloat(prompt("digite o valor do A da equação"))
+let B = parseFloat(prompt("digite o valor do B da equação"))
+let C = parseFloat(prompt("digite o valor do C da equação"))
+let delta = B^2 - 4*A*C
+console.log(`o valor do delta da equanção é ${delta}`)
