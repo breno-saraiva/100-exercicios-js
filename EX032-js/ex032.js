68e26710ca8efde916c39a6010937314ae965a04
Breno Saraiva <brenodev.saraiva@gmail.com>
2023-06-01 15:55:52 -0300

    exercício 32

---
+++ EX032-js/ex032.js
@@ -0,0 +1,8 @@
+let x = randi(5)
+let y = parseInt(prompt("escolha um numero de 1 a 5!"))
+
+if (x === y){
+    console.log("você acertou o número")
+}else {
+    console.log("voce errou o número, tente novamente")
+}
