053406abff7c6e3f1628f2343d63eb7b5af29bb1
Breno Saraiva <brenodev.saraiva@gmail.com>
2023-06-01 22:31:50 -0300

    exercício 44

---
+++ EX044-js/ex044.js
@@ -0,0 +1,8 @@
+let ni = parseInt(prompt("qual o número que irá iniciar"))
+let nf = parseInt(prompt("qual o número final"))
+let x = parseInt(prompt("valor do incremento"))
+
+while (ni <= nf) {
+    console.log(ni)
+    ni = ni + x
+}
