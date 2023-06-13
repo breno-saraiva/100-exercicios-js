3204f0e79ac2bcc874c31ebcc046749ef326c0c4
Breno Saraiva <brenodev.saraiva@gmail.com>
2023-06-13 16:13:58 -0300

    exercício 86

---
+++ EX086-js/ex086.js
@@ -0,0 +1,11 @@
+function Gerador() {
+    const linha = "+-------=======------+";
+    const mensagem = " Olá, Mundo!";
+  
+    console.log(linha);
+    console.log(mensagem);
+    console.log(linha);
+  }
+  
+  Gerador();
+  
