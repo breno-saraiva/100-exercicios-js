382697633e871f4d95d8642f29f83b597cae327c
Breno Saraiva <brenodev.saraiva@gmail.com>
2023-06-13 17:38:45 -0300

    exercício 92

---
+++ EX092-js/ex092.js
@@ -0,0 +1,11 @@
+function ParOuImpar (numero) {
+    if (numero%2 === 0 ) {
+        console.log(`${numero} é par.`)
+    } else {
+        console.log(`${numero} é ímpar.`)
+    }
+}
+
+let numero = parseInt(prompt("digite um número: "))
+
+ParOuImpar(numero)
