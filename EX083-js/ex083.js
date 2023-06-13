f54224b35e9c9ed1af5fe7055c47bf50e2a2715d
Breno Saraiva <brenodev.saraiva@gmail.com>
2023-06-13 15:40:56 -0300

    exercício 83

--- EX083-js/ex083.js
+++ EX083-js/ex083.js
@@ -1,11 +1,9 @@
 let sorteio = [];
 
-// Preenche o vetor com números aleatórios entre 0 e 99
 for (let c = 1; c <= 20; c++) {
   sorteio[c] = Math.floor(Math.random() * 100);
 }
 
-// Ordena o vetor em ordem crescente
 for (let c = 1; c <= 20; c++) {
   for (let o = c + 1; o <= 20; o++) {
     if (sorteio[c] > sorteio[o]) {
@@ -16,7 +14,6 @@ for (let c = 1; c <= 20; c++) {
   }
 }
 
-// Exibe os valores ordenados
 for (let c = 1; c <= 20; c++) {
   console.log(sorteio[c]);
 }
